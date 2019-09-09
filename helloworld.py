62e1027387c35e6f85e2691e48f592e51b2ca4f1
Jonathan Calso <jcalso@umich.edu>
2019-09-09 12:32:30 -0400

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+import random
+print("Jonathan")
+rand1 = random.randint(0, 100)
+rand2 = random.randint(0, 100)
+print(rand1)
+print(rand2)
+sum = rand1  + rand2
+print("Sum =", sum)
+average = sum / 2
+print("Average =", average)
